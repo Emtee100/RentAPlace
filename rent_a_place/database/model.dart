101ffed8cd7b4c00d89d909c45b8a03dfd86f41b
Emtee100 <mt9510037@gmail.com>
2023-06-26 23:11:12 +0300

    Schema update

--- rent_a_place/database/model.dart
+++ rent_a_place/database/model.dart
@@ -8,12 +8,39 @@ class _Users {
   late String fullName;
   late String emailAddress;
   late String photoUrl;
+  late _CreditCard? card;
+  late _MobilePay? mobilepay;
 
   late List<_Places> savedPlaces;
-
   late List<_Places> bookedPlaces;
 
   ObjectId get id => _id;
+}
+
+@RealmModel()
+class bookedPlaces {
+
+  late DateTime date;
+}
+
+@RealmModel(ObjectType.embeddedObject)
+class _CreditCard {
+  late String provider;
+  late String _cardNumber;
+  late String _expiry;
+
+  String get cardNumber => _cardNumber;
+  String get expiry => _expiry;
+
+  set setCardNumber(String value) => _cardNumber = value;
+}
+
+@RealmModel(ObjectType.embeddedObject)
+class _MobilePay {
+  late String provider;
+  late String _phoneNumber;
+  String get getNumber => _phoneNumber;
+  set setNumber(String value) => _phoneNumber = value;
 }
 
 @RealmModel()
@@ -21,10 +48,17 @@ class _Places {
   @PrimaryKey()
   @MapTo("_id")
   late ObjectId _id;
-
   late String name;
-
+  late List tags;
+  late Address address;
   late _Users bookedBy;
 
   ObjectId get id => _id;
 }
+
+@RealmModel(ObjectType.embeddedObject)
+class Address {
+  late String houseNumber;
+  late String streetName;
+}
+
